1c8424dfcff2f82a6f8902cc7997eb1be5ac946c
Rodrigo Medeiros <Rodrigo Medeiros>
2017-02-24 06:59:50 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs')
+
+
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `now: ${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err)
+            console.log('Unable to append log');
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// })
+
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        welcomeMessage: 'Welcome Friend, this is a webpage',
+        pageTitle: 'Home Page - Inicial'
+    });
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        welcomeMessage: 'What about is about',
+    });
+})
+
+app.get('/bad', (req, res) => {
+    res.send({ errorMessage: 'This is an error, request not fullfilled' });
+})
+
+app.listen(3000, () => {
+    console.log('Server is listening :3000 port');
+});
